3817a376f0dc9e520fe7296f9bc0a3002d26aec4
Mike Arpaia <mike@arpaia.co>
2018-04-14 20:05:39 -0600

    Define helpers on the underlying resource type (#23)

--- pkg/apis/lessor.io/v1/types.go
+++ pkg/apis/lessor.io/v1/types.go
@@ -1,7 +1,9 @@
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 // +genclient
@@ -17,8 +19,36 @@ type Tenant struct {
 }
 
 type TenantSpec struct {
+	Namespace    string `json:"namespace"`
 	Organization string `json:"organization"`
 	Email        string `json:"email"`
+}
+
+func (t *Tenant) Namespace() string {
+	if t.Spec.Namespace == "" {
+		return t.Name
+	}
+
+	return t.Spec.Namespace
+}
+
+func (t *Tenant) NamespaceResource() *corev1.Namespace {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            t.Namespace(),
+			OwnerReferences: t.ownerReferences(),
+		},
+	}
+}
+
+func (t *Tenant) ownerReferences() []metav1.OwnerReference {
+	return []metav1.OwnerReference{
+		*metav1.NewControllerRef(t, schema.GroupVersionKind{
+			Group:   SchemeGroupVersion.Group,
+			Version: SchemeGroupVersion.Version,
+			Kind:    "tenant",
+		}),
+	}
 }
 
 // TenantStatus is the status for a Tenant resource
